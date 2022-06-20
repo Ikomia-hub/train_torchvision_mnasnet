131b970c0219374ff9e34d106fd71e918277701f
ambroise <ambroise.berthe@ikomia.com>
2022-06-20 14:33:34 +0200

    Add test file

---
+++ train_torchvision_mnasnet_test.py
@@ -0,0 +1,19 @@
+import logging
+from ikomia.core import task, ParamMap
+from ikomia.utils.tests import run_for_test
+import os
+import ikomia
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train torchvision mnasnet =====")
+    input_path = t.getInput(0)
+    params = task.get_parameters(t)
+    params["epochs"] = 1
+    params["batch_size"] = 2
+    task.set_parameters(t, params)
+    input_path.setPath(data_dict["datasets"]["classification"]["dataset_classification"])
+    yield run_for_test(t)
