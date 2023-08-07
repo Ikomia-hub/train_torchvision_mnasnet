4179330a983b782680b1ebfedfd6f62a8bed6d17
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 17:12:34 +0200

    Fix test file

--- train_torchvision_mnasnet_test.py
+++ train_torchvision_mnasnet_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task, ParamMap
 from ikomia.utils.tests import run_for_test
 
 logger = logging.getLogger(__name__)
@@ -8,9 +7,9 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train torchvision mnasnet =====")
     input_path = t.get_input(0)
-    params = task.get_parameters(t)
-    params["epochs"] = 1
-    params["batch_size"] = 2
-    task.set_parameters(t, params)
-    input_path.setPath(data_dict["datasets"]["classification"]["dataset_classification"])
+    params = t.get_parameters()
+    params["epochs"] = "1"
+    params["batch_size"] = "2"
+    t.set_parameters(params)
+    input_path.set_path(data_dict["datasets"]["classification"]["dataset_classification"])
     yield run_for_test(t)
