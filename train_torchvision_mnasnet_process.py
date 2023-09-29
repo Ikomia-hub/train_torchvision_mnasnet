155065175d74e8e33be39a731f60aeaa0d19c709
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-09-29 09:51:02 +0200

    Chore: add metadata fields (algo type and algo tasks)

--- train_torchvision_mnasnet_process.py
+++ train_torchvision_mnasnet_process.py
@@ -123,6 +123,8 @@ class TrainMnasnetFactory(dataprocess.CTaskFactory):
         self.info.path = "Plugins/Python/Classification"
         self.info.icon_path = "icons/pytorch-logo.png"
         self.info.keywords = "MnasNet,classification,train,mobile,edge"
+        self.info.algo_type = core.AlgoType.TRAIN
+        self.info.algo_tasks = "CLASSIFICATION"
 
     def create(self, param=None):
         # Create process object
